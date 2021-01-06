0d745a6350a71e4742028b906097ab31357f19fa
Raunak <raunakb22@gmail.com>
2021-01-06 21:19:16 +0530

    Visual Help and AC button

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ let arg0 = '', arg1 = '', operator = '';
 
 digits.forEach( button => {
     button.addEventListener('click', (event) => {
-        if (event.target.getAttribute("data-type") === 'C') {
+        if (event.target.getAttribute("data-type") === 'C' ||
+            event.target.getAttribute("data-type") === 'AC') {
             screen.textContent = '';
             return;
         }
@@ -34,22 +35,45 @@ digits.forEach( button => {
 });
 
 operators[4].addEventListener('click', (event) => {
+
+    operators.forEach(el => {
+        el.style.background = '#09093B';
+        el.style.color = '#FFF';
+        el.style.boxShadow = '1px 1px 5px 5px #8080B3';
+    });
+
     if(operator === '' || arg0 === '' || screen.innerText === ''){
         screen.textContent = 'ERROR';
         return;
     }
     arg1 = +screen.innerText;
-    screen.textContent = operate(operator, arg0, arg1);
+    let result = operate(operator, arg0, arg1).toString();
+    screen.textContent = result.length < 10 ? result : result.substring(0, 10);
     arg0 = screen.textContent, arg1 = '', operator = '';
 });
 
 operators.forEach( divs => {    
     divs.addEventListener('click', (event) => {
+
         if(event.target.classList[1] === 'equal')
             return;
+
+        event.target.style.background = '#8080B3';
+        event.target.style.color = '#09093B';
+        event.target.style.boxShadow = 'none';
         arg0 = +screen.innerText; 
         operator = event.target.innerText;
         screen.textContent = '';
+
     });
     }
 );
+
+digits[12].addEventListener('click', () => {
+    arg0 = '', arg1 = '', operator = '';
+    operators.forEach(el => {
+        el.style.background = '#09093B';
+        el.style.color = '#FFF';
+        el.style.boxShadow = '1px 1px 5px 5px #8080B3';
+    });
+});
